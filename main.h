8fc41f1dc3c061853c9e94146005d3aec959a0ba
Fourteen98 <emuhiydeen6@gmail.com>
2022-05-20 17:53:28 +0000

    reduce char len for manage buffer

--- main.h
+++ main.h
@@ -9,10 +9,10 @@
 #include <limits.h>
 
 int _printf(const char * const format, ...);
-unsigned int manage_buffer(char *buffer_pointer, char c , unsigned int index_of_pointer);
+unsigned int manage_buffer(char *buffer_pointer, char c , unsigned int buffer_index);
 int _write_buffer(char *buffer, unsigned int bytes_size);
 int _write_char(va_list arg_list, char *buffer, unsigned int buffer_index);
-int _write_str(val_list arg_list, char *buffer, unsigned int buffer_index);
-int _write_int(val_list arg_list, char *buffer, unsigned int buffer_index);
+int _write_str(va_list arg_list, char *buffer, unsigned int buffer_index);
+int _write_int(va_list arg_list, char *buffer, unsigned int buffer_index);
 
 #endif
